bd7f782e0f174079b20365475adada69820df7cf
lemon <jiayihe1104@gmail.com>
2026-02-11 05:01:24 +0800

    Fix: loss functions

--- src/training/losses.py
+++ src/training/losses.py
@@ -4,13 +4,13 @@ from torchmetrics import StructuralSimilarityIndexMeasure
 from avatar_utils.config import load_config
 
 class LossFunctions(nn.Module):
-    def __init__(self, weight_rgb):
+    def __init__(self, weight_rgb=None):
         super().__init__()
         self.weight_rgb = weight_rgb if weight_rgb is not None else float(load_config().get("train", {}).get("weight_rgb", 1.0))
 
     def rgb_loss(self, pred_imgs, gt_imgs):
-        l1_loss = nn.L1Loss(mean=True)
-        ssim_loss = StructuralSimilarityIndexMeasure(data_range=1.0)  # Assuming images are normalized to [0,1]
+        l1_loss = nn.L1Loss()
+        ssim_loss = StructuralSimilarityIndexMeasure(data_range=1.0).to(pred_imgs.device)  # Assuming images are normalized to [0,1]
         return 0.8 * l1_loss(pred_imgs, gt_imgs) + 0.2 * (1 - ssim_loss(pred_imgs, gt_imgs))
 
     def forward(self, pred_imgs, gt_imgs):
